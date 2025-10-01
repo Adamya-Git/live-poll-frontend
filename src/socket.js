557ebb0c6da2ed5db772003bb1108ba8d96a7983
Adamya Gupta <adamyagupta26@gmail.com>
2025-10-01 19:11:27 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,17 @@
-// src/socket.js
-// import { io } from 'socket.io-client';
-// const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000';
-// const socket = io(URL, { autoConnect: true });
+// // src/socket.js
+// // import { io } from 'socket.io-client';
+// // const URL = process.env.REACT_APP_SERVER_URL || 'http://localhost:4000';
+// // const socket = io(URL, { autoConnect: true });
+// // export default socket;
+
+// import { io } from "socket.io-client";
+// const socket = io("https://live-poll-backend-l1is.onrender.com/"); 
+// // replace with your actual Render backend URL
 // export default socket;
 
 import { io } from "socket.io-client";
-const socket = io("https://live-poll-backend-l1is.onrender.com/"); 
-// replace with your actual Render backend URL
+
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
+const socket = io(SOCKET_URL);
+
 export default socket;
